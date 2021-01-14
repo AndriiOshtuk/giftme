49b260a2f8a1676f9645a50df97c2aed428456ce
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2021-01-14 18:07:20 +0200

    Created initial User model

--- giftme/settings.py
+++ giftme/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wishlist.apps.WishlistConfig',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50, blank=True)
+    phone_number = PhoneNumberField(help_text='Phone number')      
