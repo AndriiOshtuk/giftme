970b28103630be5ed0dc573a65c7150b8e93e498
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2021-01-14 18:45:37 +0200

    Added initial WishList and Gift models

--- wishlist/models.py
+++ wishlist/models.py
@@ -7,4 +7,26 @@ from phonenumber_field.modelfields import PhoneNumberField
 class User(models.Model):
     first_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50, blank=True)
-    phone_number = PhoneNumberField(help_text='Phone number')      
+    phone_number = PhoneNumberField(help_text='Phone number')
+
+    # TODO Add __str__()
+
+
+class WishList(models.Model):
+    name = models.CharField(max_length = 50)
+    due_date = models.DateField(blank=True, null=True)
+    user = models.ForeignKey('Gift', on_delete=models.CASCADE)
+
+    # TODO Add __str__()
+
+
+class Gift(models.Model):
+    name = models.CharField(max_length = 50)
+    description = models.TextField(help_text='Gift description, details, etc.')
+    price = models.PositiveIntegerField()
+    url = models.URLField()
+    photo = models.ImageField(upload_to='gifts_images')
+    wish_list = models.ForeignKey('WishList', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.SET_DEFAULT, default=None)
+
+    # TODO Add __str__()      
