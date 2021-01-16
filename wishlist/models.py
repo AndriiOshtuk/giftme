e5a919e9cfa88370ae0d301b2c3ffc94f6f2dc14
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2021-01-16 17:28:17 +0200

    Formatted with black

--- wishlist/models.py
+++ wishlist/models.py
@@ -5,35 +5,40 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(models.Model):
-    first_name = models.CharField(max_length = 50)
-    last_name = models.CharField(max_length = 50, blank=True)
-    phone_number = PhoneNumberField(null=False, blank=False, 
-        unique=True, help_text='Phone number')
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone_number = PhoneNumberField(
+        null=False, blank=False, unique=True, help_text="Phone number"
+    )
 
     def __str__(self):
-        return f'User:{self.first_name}:{self.phone_number}'
+        return f"User:{self.first_name}:{self.phone_number}"
 
 
 class WishList(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length=50)
     due_date = models.DateField(blank=True, null=True)
-    user = models.ForeignKey('User', on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey("User", on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return f'Wishlist:{self.name} for {self.user.first_name}'
+        return f"Wishlist:{self.name} for {self.user.first_name}"
 
     def is_due(self):
         return self.due_date < datetime.date.today()
 
 
 class Gift(models.Model):
-    name = models.CharField(max_length = 50)
-    description = models.TextField(blank=True, help_text='Gift description, details, etc.')
+    name = models.CharField(max_length=50)
+    description = models.TextField(
+        blank=True, help_text="Gift description, details, etc."
+    )
     price = models.PositiveIntegerField(blank=True, null=True)
     url = models.URLField(blank=True, null=True)
-    photo = models.ImageField(blank=True, null=True, upload_to='gifts_images')
-    wish_list = models.ForeignKey('WishList', on_delete=models.CASCADE, null=True)
-    user = models.ForeignKey('User', on_delete=models.SET_DEFAULT, default=None, null=True)
+    photo = models.ImageField(blank=True, null=True, upload_to="gifts_images")
+    wish_list = models.ForeignKey("WishList", on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(
+        "User", on_delete=models.SET_DEFAULT, default=None, null=True
+    )
 
     def __str__(self):
-        return f'Gift:{self.name} in {self.wish_list}'      
+        return f"Gift:{self.name} in {self.wish_list}"
--- wishlist/tests/test_models.py
+++ wishlist/tests/test_models.py
@@ -7,61 +7,79 @@ from wishlist.models import User, WishList, Gift
 
 
 class UserModelTests(TestCase):
-
     def create_user(self, first_name, last_name, phone_number):
-        return User.objects.create(first_name=first_name, last_name=last_name, phone_number=phone_number)
+        return User.objects.create(
+            first_name=first_name, last_name=last_name, phone_number=phone_number
+        )
 
     def test_create_simple_user(self):
-        test_user = self.create_user(first_name='John', last_name='Smith', phone_number='+41524204242')
+        test_user = self.create_user(
+            first_name="John", last_name="Smith", phone_number="+41524204242"
+        )
 
-        self.assertEquals(test_user.first_name, 'John')
-        self.assertEquals(test_user.last_name, 'Smith')
-        self.assertEquals(test_user.phone_number, '+41524204242')
+        self.assertEquals(test_user.first_name, "John")
+        self.assertEquals(test_user.last_name, "Smith")
+        self.assertEquals(test_user.phone_number, "+41524204242")
 
     def test_create_user_no_last_name(self):
-        User.objects.create(first_name='John', phone_number='+41524204242')
+        User.objects.create(first_name="John", phone_number="+41524204242")
 
         test_user = User.objects.get(id=1)
 
-        self.assertEquals(test_user.first_name, 'John')
-        self.assertEquals(test_user.last_name, '')
-        self.assertEquals(test_user.phone_number, '+41524204242')
+        self.assertEquals(test_user.first_name, "John")
+        self.assertEquals(test_user.last_name, "")
+        self.assertEquals(test_user.phone_number, "+41524204242")
 
     def test_create_user_empty_last_name(self):
-        test_user = self.create_user(first_name='John', last_name='', phone_number='+41524204242')
+        test_user = self.create_user(
+            first_name="John", last_name="", phone_number="+41524204242"
+        )
 
-        self.assertEquals(test_user.first_name, 'John')
-        self.assertEquals(test_user.last_name, '')
-        self.assertEquals(test_user.phone_number, '+41524204242')
+        self.assertEquals(test_user.first_name, "John")
+        self.assertEquals(test_user.last_name, "")
+        self.assertEquals(test_user.phone_number, "+41524204242")
 
     def test_create_none_phone_number(self):
         with self.assertRaises(IntegrityError):
-            test_user = self.create_user(first_name='John', last_name='', phone_number=None)
+            test_user = self.create_user(
+                first_name="John", last_name="", phone_number=None
+            )
 
     def test_first_name_max_length(self):
-        test_user = self.create_user(first_name='John', last_name='Smith', phone_number='+41524204242')
-        max_length = test_user._meta.get_field('first_name').max_length
+        test_user = self.create_user(
+            first_name="John", last_name="Smith", phone_number="+41524204242"
+        )
+        max_length = test_user._meta.get_field("first_name").max_length
         self.assertEquals(max_length, 50)
 
     def test_last_name_max_length(self):
-        test_user = self.create_user(first_name='John', last_name='Smith', phone_number='+41524204242')
-        max_length = test_user._meta.get_field('last_name').max_length
+        test_user = self.create_user(
+            first_name="John", last_name="Smith", phone_number="+41524204242"
+        )
+        max_length = test_user._meta.get_field("last_name").max_length
         self.assertEquals(max_length, 50)
 
 
 class WishListModelTests(TestCase):
-
     def setUp(self):
-        user1 = User.objects.create(first_name='John', last_name='Smith', phone_number='+41524204242')
-        user2 = User.objects.create(first_name='Bob', last_name='Black', phone_number='+38067300466')
+        user1 = User.objects.create(
+            first_name="John", last_name="Smith", phone_number="+41524204242"
+        )
+        user2 = User.objects.create(
+            first_name="Bob", last_name="Black", phone_number="+38067300466"
+        )
 
         date = datetime.date(2030, 12, 31)
-        WishList.objects.create(name='Gifts for NY', due_date=date, user=user1)
+        WishList.objects.create(name="Gifts for NY", due_date=date, user=user1)
         date = datetime.date(2025, 7, 4)
-        WishList.objects.create(name='Gifts for Independence Day', due_date=date, user=user1)
+        WishList.objects.create(
+            name="Gifts for Independence Day", due_date=date, user=user1
+        )
 
         date = datetime.date(2025, 7, 4)
-        WishList.objects.create(name='Gifts for Independence Day', due_date=date, user=user2)
+        WishList.objects.create(
+            name="Gifts for Independence Day", due_date=date, user=user2
+        )
 
     def test_cascade_delete(self):
         test_user = User.objects.get(id=2)
@@ -71,32 +89,37 @@ class WishListModelTests(TestCase):
 
     def test_last_name_max_length(self):
         wishlist = WishList.objects.get(id=1)
-        max_length = wishlist._meta.get_field('name').max_length
+        max_length = wishlist._meta.get_field("name").max_length
         self.assertEquals(max_length, 50)
 
     def test_is_due_passed_date(self):
         date = datetime.date.today() - datetime.timedelta(days=1)
-        wishlist = WishList(name='Wishlist with due date', due_date=date)
+        wishlist = WishList(name="Wishlist with due date", due_date=date)
         self.assertIs(wishlist.is_due(), True)
 
     def test_is_due_today_date(self):
         date = datetime.date.today()
-        wishlist = WishList(name='Wishlist with due date', due_date=date)
+        wishlist = WishList(name="Wishlist with due date", due_date=date)
         self.assertIs(wishlist.is_due(), False)
 
     def test_is_due_tomorrow_date(self):
         date = datetime.date.today() + datetime.timedelta(days=1)
-        wishlist = WishList(name='Wishlist with due date', due_date=date)
+        wishlist = WishList(name="Wishlist with due date", due_date=date)
         self.assertIs(wishlist.is_due(), False)
 
 
 class GiftModelTests(TestCase):
-
     def setUp(self):
-        self.user1 = User.objects.create(first_name='John', last_name='Smith', phone_number='+41524204242')
+        self.user1 = User.objects.create(
+            first_name="John", last_name="Smith", phone_number="+41524204242"
+        )
         date = datetime.date(2030, 12, 31)
-        self.wishlist1 = WishList.objects.create(name='Gifts for NY', due_date=date, user=self.user1)
-        self.gift1 = Gift.objects.create(name='Gift 1', wish_list=self.wishlist1, user=self.user1)
+        self.wishlist1 = WishList.objects.create(
+            name="Gifts for NY", due_date=date, user=self.user1
+        )
+        self.gift1 = Gift.objects.create(
+            name="Gift 1", wish_list=self.wishlist1, user=self.user1
+        )
 
     def test_remove_wishlist_removes_gift(self):
         wishlist = WishList.objects.get(id=1)
@@ -110,16 +133,19 @@ class GiftModelTests(TestCase):
         gifts = Gift.objects.count()
         self.assertEquals(gifts, 0)
 
-
     def test_name_max_length(self):
         gift = Gift.objects.get(id=1)
-        max_length = gift._meta.get_field('name').max_length
+        max_length = gift._meta.get_field("name").max_length
         self.assertEquals(max_length, 50)
 
     def test_price_positive_ok(self):
-        gift = Gift.objects.create(name='Gift 1', price=5, wish_list=self.wishlist1, user=self.user1)
+        gift = Gift.objects.create(
+            name="Gift 1", price=5, wish_list=self.wishlist1, user=self.user1
+        )
         self.assertEquals(gift.price, 5)
 
     def test_price_error_on_negative(self):
         with self.assertRaises(IntegrityError):
-            Gift.objects.create(name='Gift 1', price=-5, wish_list=self.wishlist1, user=self.user1)
+            Gift.objects.create(
+                name="Gift 1", price=-5, wish_list=self.wishlist1, user=self.user1
+            )
