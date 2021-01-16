44f6d58300e0be3c182747571f09619aacc50418
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2021-01-16 16:25:20 +0200

    Modified fields of Gift model to optional

--- wishlist/models.py
+++ wishlist/models.py
@@ -17,10 +17,10 @@ class User(models.Model):
 class WishList(models.Model):
     name = models.CharField(max_length = 50)
     due_date = models.DateField(blank=True, null=True)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return f'Wishlist:{self.name} for {self.user.name}'
+        return f'Wishlist:{self.name} for {self.user.first_name}'
 
     def is_due(self):
         return self.due_date < datetime.date.today()
@@ -28,12 +28,12 @@ class WishList(models.Model):
 
 class Gift(models.Model):
     name = models.CharField(max_length = 50)
-    description = models.TextField(help_text='Gift description, details, etc.')
-    price = models.PositiveIntegerField()
-    url = models.URLField()
-    photo = models.ImageField(upload_to='gifts_images')
-    wish_list = models.ForeignKey('WishList', on_delete=models.CASCADE)
-    user = models.ForeignKey('User', on_delete=models.SET_DEFAULT, default=None)
+    description = models.TextField(blank=True, help_text='Gift description, details, etc.')
+    price = models.PositiveIntegerField(blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+    photo = models.ImageField(blank=True, null=True, upload_to='gifts_images')
+    wish_list = models.ForeignKey('WishList', on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey('User', on_delete=models.SET_DEFAULT, default=None, null=True)
 
     def __str__(self):
         return f'Gift:{self.name} in {self.wish_list}'      
