c26e8cae685eb2d4c8a3ac743e56cf0811d08723
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2021-03-16 22:32:48 +0200

    Set up debug

--- giftme/settings.py
+++ giftme/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('GIFTME_SECRET_KEY', 's3cr3t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', False)
 
 ALLOWED_HOSTS = ["*"]
 
